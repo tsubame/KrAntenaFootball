2326e889a1bc29b112ee3eb32b19e89fc6a6ba5a
hideki <hideki@iMacT.local>
2013-09-01 18:55:16 +0900

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KrAntenaFootball::Application.config.secret_token = 'b84fd3cda69be2b3d1cf993be1b56826e976a3b72211fbf440869e30246eda38c6f40ee17296deaacb53ade43cfd810deea4399d043750cd47f691326659a05b'
